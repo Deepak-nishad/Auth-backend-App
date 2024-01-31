7264cfb924897b34f717d0a57eddb26057877476
Deepak-nishad <deepakkumar62559@gmail.com>
2024-02-01 01:48:16 +0530

    auth app complete

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const { connect } = require("./config/database");
+const cookieParser = require("cookie-parser");
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.Port || 5000;
+app.use(express.json());
+
+
+app.use(cookieParser());
+const user = require("./routes/user");
+app.use("/api/v1", user);
+
+app.listen(PORT, () => {
+    console.log(`server is running at ${PORT}`);
+})
+
+connect();
+
+app.get("/", (req, res) => {
+    res.send(`<h1> This is home page </h1>`)
+})
