69e9c7a3ba0541cf6b1ca0667c3fc73c06599128
Deepak-nishad <deepakkumar62559@gmail.com>
2024-02-01 13:47:46 +0530

    cors add

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 
 const { connect } = require("./config/database");
+const cors = require('cors');
 const cookieParser = require("cookie-parser");
+
+app.use(cors());
 const app = express();
 require("dotenv").config();
 
